e1abd316df02655e77eb605a6c0183b47aab92a9
Joshua Levy <joshua.levy676@gmail.com>
2021-09-14 10:30:48 -0500

    Joshua thinks this works, please review

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,5 +1,3 @@
-test = 7
-
 def steps(number):
     presentStep = 0
     n = number
@@ -7,17 +5,15 @@ def steps(number):
     while n != 1:
         if number > 1:
             # n is even
-            if n % 2== 0:
+            if n % 2 == 0:
                 n = n /2
                 presentStep +=1 
             # n is odd:
             else: 
                 n = 3*n+1
                 presentStep +=1
+        # If n is 0 or negative throw an error
         else:
             raise ValueError
 
-    print(presentStep)
     return presentStep
-
-steps(test)
