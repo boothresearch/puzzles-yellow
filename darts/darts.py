40f6f984efd0ce817775122b0260d723e8405b05
kdaehler <katherine.daehler@chicagobooth.edu>
2021-09-14 15:33:23 +0000

    Completed darts

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,14 @@
 def score(x, y):
-    pass
+    def squared(num):
+        return num ** 2
+    if  squared(x) + squared(y) > 100:
+        return 0 
+    elif  (squared(x) + squared(y) <= 100) and (squared(x) + squared(y) > 25):
+        return 1
+    elif  (squared(x) + squared(y) <= 25) and (squared(x) + squared(y) > 1):
+        return 5
+    elif  squared(x) + squared(y) <= 1:
+        return 10
+
+
+
