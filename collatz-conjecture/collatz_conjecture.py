5713e4efd6a683f5f0c76237d4b6f51695159383
Joshua Levy <joshua.levy676@gmail.com>
2021-09-14 10:25:30 -0500

    Starting on collatz

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,23 @@
+test = 7
+
 def steps(number):
-    pass
+    presentStep = 0
+    n = number
+
+    while n != 1:
+        if number > 1:
+            # n is even
+            if n % 2== 0:
+                n = n /2
+                presentStep +=1 
+            # n is odd:
+            else: 
+                n = 3*n+1
+                presentStep +=1
+        else:
+            raise ValueError
+
+    print(presentStep)
+    return presentStep
+
+steps(test)
